be1a8a8cf5d385b783f240b68fa6a6555a0e0c1e
harry1932763 <70296195+harry1932763@users.noreply.github.com>
2021-11-27 03:01:41 +0000

    re wrote everything, a bit more concise

--- main.c
+++ main.c
@@ -1,115 +1,104 @@
-// first attempt at combine backwards, 26/11/21
+/* Author : Harry Dean
+   Date.  : 27/11/21
+   version: 1.1
+*/  
+
+
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-
-const char * combine_backwards(char sentence1[], char sentence2[]);
-
+const char * merge_reverse(char input_sentence_1[],char input_sentence_2[]); 
 
 int main(void) {
-
-  // allocate space for each sentence with 500chars of memory
-  char *sentence_1 = malloc(500 * sizeof(char));
-  char *sentence_2 = malloc(500 * sizeof(char));
-  // prompt the user for 2 sentences 
-  printf("enter sentence 1:\n");
-  // each character is scanned and stored in sentence_1 and sentence_2, and stops when a newline is found the \n char is read but is not included in the scan so that when scanf is used again it doesn't just stop due to it always reading the \n character. 
-  scanf("%[^\n]%*c", sentence_1 );
-  printf("\nenter sentence 2:\n");
-  scanf("%[^\n]%*c", sentence_2 );
-  // print the sentences without editing them. 
-  printf("\nsentence1 %s\nsentence2 %s", sentence_1, sentence_2);
-  // find the length of the senetences to enable a more precise amount of memory for the 'pointer' so that it has enough space to store both sentences and an extra one for the NULL character.  
-  int length1 = strlen(sentence_1);
-  int length2 = strlen(sentence_2); 
-  char *pointer = malloc((length1 + length2 +1)* sizeof(char));
-  // copy the result of combine_backwards onto pointer as memory was allocated in the combine combine_backwards function and it needs to be freed, so now 'pointer' and 'reversed_combined' pointers point to the same memory so freeing 1 of them frees both.  
-  strcpy(pointer, combine_backwards(sentence_1 , sentence_2));
-  // print the results of the reversed and mearged sentences. 
-  printf("\n%s", combine_backwards(sentence_1 , sentence_2));
-
-  //any time malloc is used it must be freed at some point to prevent memory leaks, this is the last part of the main function so any unfreed memory has to be freed here. 
-  free(pointer);
-  free(sentence_1);
-  free(sentence_2);
-
+  // varibles to store user inputted sentences
+  char input_sentence_1[100];
+  char input_sentence_2[100]; 
+  // since the merge_reverse returns a pointer another pointer pointing to the same thing has to be used and freed in the main function. 
+  char *result_of_merge = malloc(200 * sizeof(char));
+  // prompt user for sentences
+  printf("\nenter first sentence\n");
+  // scan console until a newline is found, %*c reads the \n but doesn't scan it into input_sentence_1, this is to prevent scanf("%[^\n]") from cancelling imidietly. 
+  scanf("%[^\n]%*c", input_sentence_1);
+  printf("enter second sentence\n");
+  scanf("%[^\n]%*c", input_sentence_2);
+  // print the users sentences for easier comparison and comfirmation of the code working
+  printf("sentence 1:            %s\nsentence 2:            %s", input_sentence_1, input_sentence_2);
+  // the memory needs to be freed from reversed lst which is stored onto result_of_merge so freeing result_of_merge which points to the same memory as reversed_lst frees both. 
+  strcpy(result_of_merge, merge_reverse(input_sentence_1,input_sentence_2)); 
+  // print the result which was stored in result_of_merge 
+  printf("\nmerged and reversed  = %s", result_of_merge);
+  // free the memory that result_of_merge is pointing to. 
+  free(result_of_merge); 
   return 0;
 }
 
-const char * combine_backwards(char sentence1[], char sentence2[]){
-  
-  // declare variables 
-  // lengths of input sentences are stored to allocate enough memory without having an excess amount
-  int sentence1_length = strlen(sentence1);
-  int sentence2_length = strlen(sentence2);
-  
-  // shortest and longest are used when deciding how the lists will be mearged as the longer list will have it's excess added on at the end. 
-  int shortest, longest;
-  // counters have been used to track positions in arrays, this was probably possible with for-loops but decided this was the easier route for me. 
-  int counter_1 = 0;
-  int counter_2 = 0;
-  int counter_3, counter_4;
-  // bigger_sentence is used to make it a big more obvious where the sentences have stopped merging and show where the unmerged part is being added. 
-  char *bigger_sentence = malloc((sentence1_length+sentence2_length+1)*sizeof(char)); 
-  // this is storage allocated for the reversed result of the merged sentences 
-  char *reversed_combined= malloc((sentence1_length+sentence2_length+1)*sizeof(char)); 
 
-  // printf("\nlengths %d %d", sentence1_length, sentence2_length);
-  char *combined_lst = malloc((sentence1_length+sentence2_length+1)*sizeof(char));
-  // this is where i've determined whih sentence is longer and have set their lengths to shortest and longestffd this information is used to work out when to stop merging the lists as the shorter one will run out of characters to merge. 
-  if (sentence1_length < sentence2_length) {
-    shortest = sentence1_length;
-    longest = sentence2_length;
-    bigger_sentence = sentence2; 
-
+const char * merge_reverse(char input_sentence_1[],char input_sentence_2[]){
+  // declare variables
+  // smaller_lst & longer_lst are used to determine which list is bigger, 
+  // the lengths are also used for the same reason 
+  int smaller_lst, longer_lst, length_sentence_1, length_sentence_2; 
+  // store the length value onto the variables.  
+  length_sentence_1 = strlen(input_sentence_1);
+  length_sentence_2 = strlen(input_sentence_2); 
+  // biggest_lst will be used to deal with the last part of the array which can't be merged with the other due to it being shorter. 
+  char *biggest_lst = malloc((length_sentence_1+length_sentence_2+1)*sizeof(char));
+  // combined_lst will store the merged list of sentence_1 and sentence_2
+  char *combined_lst = malloc((length_sentence_1+length_sentence_2+1)*sizeof(char)); 
+  // this will store the reverse of the combined_lst
+  char *reversed_lst = malloc((length_sentence_1+length_sentence_2+1)*sizeof(char)); 
+  // this if else statement will determine which sentence is the bigger and smaller one and copy the sentence on to the biggest_lst 
+  if (length_sentence_1 > length_sentence_2){
+    smaller_lst = length_sentence_2;
+    longer_lst = length_sentence_1;
+    strcpy(biggest_lst, input_sentence_1);
   }
   else{
-    shortest = sentence2_length;
-    longest = sentence1_length;
-    bigger_sentence = sentence1;
+    smaller_lst = length_sentence_1;
+    longer_lst = length_sentence_2;
+    strcpy(biggest_lst, input_sentence_1);
   }
-  
 
-  // since the merged part is going to be twice that of the shortest sentence the for - loop goes from 0 to shortest*2 
-  for (int i = 0; i<shortest*2; i++ ){
-    // this if statement lets me alternate between lists so that each list will store 1 item after the other until. 
+  //counters to through the input sentences 1 index at a time. 
+  int sentence_1_counter = 0;
+  int sentence_2_counter = 0;
+  // smaller_lst*2 has been choosen as the two sentences will merge as long as they are equal length i've decided to sort out the merging parts first and then add the last part of the bigger file onto the end. 
+  for (int i = 0; i < smaller_lst*2; i++){
+    // this if stament acts as a switch every time 'i' is even the sentence_1 index gets added to the combined list and when 'i' is odd the sentence_2 gets added, taking turn which results in the merge. 
     if (i % 2 == 0){
-      // stores the letters from sentence_1 in the combined_lst
-    combined_lst[i] = sentence1[counter_1];
-    // counter is used to remember position in the array
-    counter_1++;
+    combined_lst[i] = input_sentence_1[sentence_1_counter];
+    // increament the counter so the next index of sentnce_1 is added each time 
+    sentence_1_counter++;
     }
-    else {
-      // stores letters from sentence_2 in the combined_lst 
-    combined_lst[i] = sentence2[counter_2];
-    counter_2++;
+    else{
+      combined_lst[i] = input_sentence_2[sentence_2_counter];
+      // increament the counter so the next index of sentnce_2 is added each time 
+      sentence_2_counter++;
     }
   }
-  // in the previous statements the two sentences were combined up until the shortest length they share, 
-  // counter_3 is going to be used to add items to the next index from where they stopped merging.  
-  counter_3 = shortest*2;
-  for(int i = shortest; i < longest; i++){
-    // here the last part of the bigger_sentence is added onto the end of the combined_lst.
-    combined_lst[counter_3] = bigger_sentence[i];
-    counter_3++; 
+  // the biggest_lst_counter is equal to the smaller_lst and that's where the merge stopped happing.
+  int biggest_lst_counter = smaller_lst; 
+  for (int i = smaller_lst*2; i < (length_sentence_1+length_sentence_2); i++){
+    // adding the last parts of the bigger_lst to the combined_lst 1 item at a time.
+    combined_lst[i] = biggest_lst[biggest_lst_counter];
+    // increament the counter so the next index of biggest_lst is added each time
+    biggest_lst_counter++; 
   }
-
-  int combined_length = strlen(combined_lst);
+  // printf("\n%s", combined_lst); 
+  // find the length of this combined list as we're going to need to do a for-loop from it's last index to the first, the last index should be equal to it's combined_length
+    int combined_length = strlen(combined_lst);
   // counter is used to keep track of array position.
-  counter_4 = 0; 
+  int counter_4 = 0; 
   // for loop is used to store the combined_lst in reversed order to the reversed_combined storage. 
   for (int i = combined_length-1; i>=0 ;i--){
-    // printf("\n%c", combined_lst[i]);
-    reversed_combined[counter_4] = combined_lst[i];
+    // store each combined_lst 1 item at a time in reversed_lst. 
+    reversed_lst[counter_4] = combined_lst[i];
     counter_4++;
-
   }
-  
-  // memory is freed. 
-  free(combined_lst);
-
-  char *final_result = reversed_combined; 
-  return final_result; 
-}
+  // free memory as any memory allocated needs to be freed by free()
+  free(biggest_lst);
+  free(combined_lst); 
+  return reversed_lst;
+} 
